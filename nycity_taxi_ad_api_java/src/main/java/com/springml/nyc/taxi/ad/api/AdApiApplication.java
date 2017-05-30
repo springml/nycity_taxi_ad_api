c7a6eac801dd8e39b6eeca8015ed8f28d5c359a4
Samuel Alexander <samuel@palmtreeinfotech.com>
2017-05-30 18:32:25 +0530

    Swagger enabled

--- nycity_taxi_ad_api_java/src/main/java/com/springml/nyc/taxi/ad/api/AdApiApplication.java
+++ nycity_taxi_ad_api_java/src/main/java/com/springml/nyc/taxi/ad/api/AdApiApplication.java
@@ -3,11 +3,18 @@ package com.springml.nyc.taxi.ad.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created by sam on 28/4/17.
  */
 @SpringBootApplication
+@EnableSwagger2
 public class AdApiApplication {
     public static void main(String args[]) {
         SpringApplication.run(AdApiApplication.class, args);
@@ -21,5 +28,17 @@ public class AdApiApplication {
     @Bean
     public AdThresholdService adThresholdService() {
         return new AdThresholdService();
+    }
+
+    @Bean
+    public Docket swaggerDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfo("Coupon Service", "Coupon Service", "1.0", null,
+                        null, "Apache 2.0",
+                        "http://www.apache.org/licenses/LICENSE-2.0"))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
--- nycity_taxi_ad_api_java/src/main/java/com/springml/nyc/taxi/ad/api/AdResource.java
+++ nycity_taxi_ad_api_java/src/main/java/com/springml/nyc/taxi/ad/api/AdResource.java
@@ -2,6 +2,7 @@ package com.springml.nyc.taxi.ad.api;
 
 import com.springml.nyc.taxi.ad.api.model.Coupon;
 import com.springml.nyc.taxi.ad.api.model.RideDetails;
+import io.swagger.annotations.Api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,17 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 /**
  * Created by sam on 28/4/17.
  */
 @RestController
+@Api(value="Coupon Service", description="Fetches the coupon based on CloudML response")
 public class AdResource {
     private static final Logger LOG = LoggerFactory.getLogger(AdResource.class);
 
     @Autowired
     private AdServer adServer;
-    @RequestMapping("/")
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ApiIgnore
     public String home() {
         return "Coupon Service is running!";
     }
@@ -40,7 +45,8 @@ public class AdResource {
      * <a href="https://cloud.google.com/appengine/docs/flexible/java/how-instances-are-managed#health_checking">
      * App Engine health checking</a> requires responding with 200 to {@code /_ah/health}.
      */
-    @RequestMapping("/_ah/health")
+    @ApiIgnore
+    @RequestMapping(value = "/_ah/health", method = RequestMethod.GET)
     public String healthy() {
         return "OK";
     }
